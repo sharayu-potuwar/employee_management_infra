4b98e02176e9e6fd64a13cd597bbdcbeaff94f67
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-14 21:12:42 -0500

    cloudbuild resource provisioned

---
+++ terraform/cloudbuild.tf
@@ -0,0 +1,42 @@
+// Create a secret containing the personal access token and grant permissions to the Service Agent
+resource "google_secret_manager_secret" "github_token_secret" {
+    project =  "gcp-devops-436118"
+    secret_id = "github-secret"
+
+    replication {
+        auto {}
+    }
+}
+
+resource "google_secret_manager_secret_version" "github_token_secret_version" {
+    secret = google_secret_manager_secret.github_token_secret.id
+    secret_data = "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"
+}
+
+data "google_iam_policy" "serviceagent_secretAccessor" {
+    binding {
+        role = "roles/secretmanager.secretAccessor"
+        members = ["serviceAccount:service-gcp-devops-436118@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
+    }
+}
+
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  project = google_secret_manager_secret.github_token_secret.project
+  secret_id = google_secret_manager_secret.github_token_secret.secret_id
+  policy_data = data.google_iam_policy.serviceagent_secretAccessor.policy_data
+}
+
+// Create the GitHub connection
+resource "google_cloudbuildv2_connection" "my_connection" {
+    project = "gcp-devops-436118"
+    location = "us-central1"
+    name = "cloudbuild-github-conn"
+
+    github_config {
+        app_installation_id = 55361269
+        authorizer_credential {
+            oauth_token_secret_version = google_secret_manager_secret_version.github_token_secret_version.id
+        }
+    }
+    depends_on = [google_secret_manager_secret_iam_policy.policy]
+}

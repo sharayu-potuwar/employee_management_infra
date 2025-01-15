9d61def1df250e608401acdc87d93662a1c8d535
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-14 22:57:59 -0500

    artifact registry of docker complaint is crated

---
+++ terraform/artifactregistry.tf
@@ -0,0 +1,10 @@
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "employee-repository"
+  description   = "This repository is created for docker images of employee app and cloudsql auth proxy"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}

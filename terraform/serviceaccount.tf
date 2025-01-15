614fc210fa2c09d7d3998f4f3e75567e7f7aa8c4
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-14 21:24:51 -0500

    project number corrected in google-iam-policy

--- terraform/serviceaccount.tf
+++ terraform/serviceaccount.tf
@@ -2,3 +2,8 @@ resource "google_service_account" "service_account" {
   account_id   = "sa-cloudsql-01"
   display_name = "sa-cloudsql-01"
 }
+
+resource "google_service_account" "service_account01" {
+  account_id   = "sa-cloudbuild-01"
+  display_name = "sa-cloudbuild-01"
+}

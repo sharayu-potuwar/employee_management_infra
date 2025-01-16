053232a60527a733b4bba2ab1f0e3f68f313c573
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-16 18:29:45 -0500

    delivery pipeline provisioned

--- terraform/clouddeploy.tf
+++ terraform/clouddeploy.tf
@@ -54,3 +54,39 @@ resource "google_clouddeploy_target" "primary" {
     depends_on = [ google_container_cluster.primary ]
 }
 
+
+resource "google_clouddeploy_delivery_pipeline" "primary" {
+  location    = "us-west1"
+  name        = "pipeline"
+  description = "This delivery pipeline for employee application"
+  project     = "gcp-devops-436118"
+
+  serial_pipeline {
+    stages {
+    #  deploy_parameters {
+    #     values = {
+    #       deployParameterKey = "deployParameterValue"
+    #     } 
+
+        # match_target_labels = {}
+    #   }
+
+    #   profiles  = ["example-profile-one", "example-profile-two"]
+      target_id = google_clouddeploy_target.primary.id
+    }
+
+     }
+
+#   annotations = {
+#     my_first_annotation = "example-annotation-1"
+
+#     my_second_annotation = "example-annotation-2"
+#   }
+
+#   labels = {
+#     my_first_label = "example-label-1"
+
+#     my_second_label = "example-label-2"
+#   }
+#   provider    = google-beta
+}
